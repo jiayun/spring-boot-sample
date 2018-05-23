3862c5d97bec262e908f416bfd91847915be8f84
Claudia Zhou <claudia.zhou@104.com.tw>
2018-05-23 13:26:42 +0800

    Multiple Datasources

---
+++ src/main/java/com/example/demo/DbConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "demo2DataSource")
+    @ConfigurationProperties(prefix = "demo2.datasource")
+    public DataSource demo2DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
---
+++ src/main/java/com/example/demo/dao/Demo2Repository.java
@@ -0,0 +1,23 @@
+package com.example.demo.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class Demo2Repository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Autowired
+    public void setDataSource(@Qualifier("demo2DataSource") DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+}
